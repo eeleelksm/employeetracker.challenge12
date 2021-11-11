ecc9ed007c13a908567367a9256b5a81e759658d
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-11 15:54:21 -0500

    package update

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ const inquirer = require("inquirer");
 const db = require("./db/connection");
 const figlet = require("figlet");
 
-const appHeader = () => {
-  figlet.text('My Employee Tracker', {
-    font: 'banner',
-    horizontalLayout: 'default',
-    verticalLayout: 'fitted',
-    width: 80,
-    whitespaceBreak: true
-  }, function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data);
-  });
-};
+// const appHeader = () => {
+//   figlet.text('My Employee Tracker', {
+//     font: 'banner',
+//     horizontalLayout: 'default',
+//     verticalLayout: 'fitted',
+//     width: 80,
+//     whitespaceBreak: true
+//   }, function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data);
+//   });
+// };
 
 const employeeTracker = () => {
+  console.log(`
+  Business Employee Tracker
+  =========================
+  `)
 
-  return inquirer.prompt([
+  inquirer.prompt([
       {
         name:"user_name",
         type: "input",
